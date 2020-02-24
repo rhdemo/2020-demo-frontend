bebd4002fec6f53ec70589330d2fc4110ffc25f0
Chris Chase <cchase@redhat.com>
2020-02-24 16:50:04 -0500

    Cleanup.

--- dashboard-server/utils/storage.js
+++
@@ -1,7 +0,0 @@
-const env = require("env-var");
-const S3Storage = require("./s3Storage");
-
-const {S3_ENDPOINT, S3_BUCKET, S3_PREFIX, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY} = require("./constants");
-const storage = new S3Storage(S3_ENDPOINT, S3_BUCKET, S3_PREFIX, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY);
-
-module.exports = storage;
