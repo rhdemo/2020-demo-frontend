aa3344badc38143a17352a586c475b058af5445c
Kyle Buchanan <kylebuch8@gmail.com>
2020-02-27 20:27:07 -0500

    main screen style updates

--- phone-ui/src/Main/components/Main.js
+++ phone-ui/src/Main/components/Main.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { sendPing, sendGuess } from "../actions";
 import { Droppable } from "@shopify/draggable";
 
 import "./Main.scss";
 
-function Main({ player, currentRound, sendPing, answer, game, sendGuess }) {
+function Main({ player, currentRound, sendPing, game, sendGuess }) {
   const ref = React.createRef();
-  const [droppable, setDroppable] = useState();
-  const [droppableListenersSet, setDroppableListenersSet] = useState(false);
+  const currentRoundState = useRef(currentRound);
 
   useEffect(() => {
     const droppable = new Droppable(
@@ -19,21 +18,25 @@ function Main({ player, currentRound, sendPing, answer, game, sendGuess }) {
       }
     );
 
-    // setDroppable(droppable);
-
     droppable.on("drag:stop", event => {
       const droppableDestination = event.source.parentNode;
       const destinationIndex = droppableDestination.dataset.index;
       const droppedIndex = event.source.dataset.index;
 
-      let choices = [...currentRound.choices];
-      let answers = currentRound.answers.map(({ format, number }) => ({
-        format,
-        number
-      }));
+      if (!droppableDestination.classList.contains("guess")) {
+        return;
+      }
+
+      let choices = [...currentRoundState.current.choices];
+      let answers = currentRoundState.current.answers.map(
+        ({ format, number }) => ({
+          format,
+          number
+        })
+      );
 
       let guess = {
-        itemId: currentRound.itemId,
+        itemId: currentRoundState.current.itemId,
         playerId: player.id,
         gameId: game.id,
         choices,
@@ -44,113 +47,109 @@ function Main({ player, currentRound, sendPing, answer, game, sendGuess }) {
 
       sendGuess(guess);
     });
-    //
-    // // droppable.on("drag:stop", dragStopHandler);
-    //
-    // droppable.on("droppable:dropped", event => {
-    //   if (!event.dropzone.classList.contains("guess")) {
-    //     event.cancel();
-    //     return;
-    //   }
-    //
-    //   if (event.dropzone.classList.contains("draggable-dropzone--occupied")) {
-    //     event.cancel();
-    //     return;
-    //   }
-    // });
+
+    droppable.on("droppable:dropped", event => {
+      if (!event.dropzone.classList.contains("guess")) {
+        event.cancel();
+        return;
+      }
+
+      if (event.dropzone.classList.contains("draggable-dropzone--occupied")) {
+        event.cancel();
+        return;
+      }
+    });
   }, []);
 
   useEffect(() => {
-    // if (!droppable) {
-    //   return;
-    // }
-    //
-    // function dragStopHandler(event) {
-    //   const droppableDestination = event.source.parentNode;
-    //   const destinationIndex = droppableDestination.dataset.index;
-    //   const droppedIndex = event.source.dataset.index;
-    //
-    //   console.log(currentRound);
-    //   let choices = [...currentRound.choices];
-    //   let answers = currentRound.answers.map(({ format, number }) => ({
-    //     format,
-    //     number
-    //   }));
-    //
-    //   let guess = {
-    //     itemId: currentRound.itemId,
-    //     playerId: player.id,
-    //     gameId: game.id,
-    //     choices,
-    //     source: droppedIndex,
-    //     answers,
-    //     destination: destinationIndex
-    //   };
-    //
-    //   sendGuess(guess);
-    // }
-    //
-    // if (!droppableListenersSet) {
-    //   droppable.on("drag:stop", dragStopHandler);
-    //   setDroppableListenersSet(true);
-    // }
-  }, [droppable, currentRound]);
+    currentRound.answers.forEach((answer, index) => {
+      if (answer.result === "incorrect") {
+        const incorrectGuessDropzone = ref.current.querySelector(
+          `.number-input .guess.dropzone[data-index="${index}"]`
+        );
+        const incorrectGuess = incorrectGuessDropzone.querySelector(
+          ".choice.item"
+        );
+        const originalGuessLocation = ref.current.querySelector(
+          `.choices .dropzone[data-index="${parseInt(answer.from, 10)}"]`
+        );
 
-  useEffect(() => {
-    // currentRound.answers.forEach((answer, index) => {
-    //   if (answer.result === "incorrect") {
-    //     const incorrectGuessDropzone = document.querySelector(
-    //       `.number-input .guess.dropzone[data-index="${index}"]`
-    //     );
-    //     const incorrectGuess = incorrectGuessDropzone.querySelector(
-    //       ".choice.item"
-    //     );
-    //     const originalGuessLocation = document.querySelector(
-    //       `.choices .dropzone[data-index="${parseInt(answer.from, 10)}"]`
-    //     );
-    //
-    //     originalGuessLocation.appendChild(incorrectGuess);
-    //     incorrectGuessDropzone.classList.remove("draggable-dropzone--occupied");
-    //     originalGuessLocation.classList.add("draggable-dropzone--occupied");
-    //   }
-    //
-    //   if (answer.result === "correct") {
-    //     // const correctGuessDropzone = document.querySelector(`.number-input .guess[data-index="${index}"]`);
-    //     // const originalDiv = document.querySelector(`.number-input .guess .choice.item[data-index="${answer.from}"]`);
-    //     // if (correctGuessDropzone && originalDiv) {
-    //     //   correctGuessDropzone.removeChild(originalDiv);
-    //     // }
-    //   }
-    // });
+        if (originalGuessLocation && incorrectGuess) {
+          originalGuessLocation.appendChild(incorrectGuess);
+          incorrectGuessDropzone.classList.remove(
+            "draggable-dropzone--occupied"
+          );
+          originalGuessLocation.classList.add("draggable-dropzone--occupied");
+        }
+      }
+
+      if (answer.result === "correct") {
+        const correctGuessDropzone = ref.current.querySelector(
+          `.number-input .guess[data-index="${index}"]`
+        );
+        const originalDiv = ref.current.querySelector(
+          `.number-input .guess .choice.item[data-index="${answer.from}"]`
+        );
+
+        if (correctGuessDropzone && originalDiv) {
+          correctGuessDropzone.removeChild(originalDiv);
+        }
+      }
+    });
+
+    if (currentRoundState.current.itemId !== currentRound.itemId) {
+      [...ref.current.querySelectorAll(".guess.dropzone")].forEach(dropzone => {
+        dropzone.innerHTML = null;
+        dropzone.classList.remove("draggable-dropzone--occupied");
+      });
+    }
+
+    currentRoundState.current = currentRound;
   }, [currentRound]);
+
+  const imageBackground = {
+    backgroundImage: `url(${currentRound.image})`
+  };
+
+  const playerName = <span>{player.username}</span>;
+  const pointsSuffix = player.score === 1 ? "" : "s";
 
   return (
     <div className="main">
       <header>
-        <h1>Lando Calrissian</h1>
+        <h1>{playerName}</h1>
+        <h2>
+          {player.score} point{pointsSuffix}
+        </h2>
       </header>
       <main>
         <div className="mountains"></div>
         <div className="content" ref={ref}>
-          <div className="image"></div>
+          <div className="image">
+            <div className="image-background" style={imageBackground}></div>
+          </div>
           <div className="number-input container">
             <h2>$</h2>
             {currentRound.answers.map((answer, index) => {
               if (answer.format === "decimal") {
-                return <div key={index}>.</div>;
+                return <div key={currentRound.itemId + "-" + index}>.</div>;
               }
 
-              // if (answer.result === "correct") {
-              //   return (
-              //     <div className="guess" key={index} data-index={index}>
-              //       <div className="choice item">
-              //         <div className="bg">
-              //           <div className="number">{answer.number}</div>
-              //         </div>
-              //       </div>
-              //     </div>
-              //   )
-              // }
+              if (answer.result === "correct") {
+                return (
+                  <div
+                    className="guess"
+                    key={currentRound.itemId + "-" + index}
+                    data-index={index}
+                  >
+                    <div className="choice item">
+                      <div className="bg">
+                        <div className="number">{answer.number}</div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
 
               return (
                 <div
@@ -167,7 +166,7 @@ function Main({ player, currentRound, sendPing, answer, game, sendGuess }) {
                 className="dropzone draggable-dropzone--occupied"
                 data-index={index}
                 data-number={choice}
-                key={index}
+                key={currentRound.itemId + "-" + index}
               >
                 <div
                   className="choice item"
