cbfae958a4d8ada0ec9c554d5ef41f05b93b0c39
Kyle Buchanan <kylebuch8@gmail.com>
2020-03-04 21:43:57 -0500

    new header component

---
+++ phone-ui/src/Header/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import "./Header.scss";
+
+function Header({ player }) {
+  const pointsSuffix = player.score === 1 ? "" : "s";
+  const playerNameArray = player.username.split("");
+  const displayArray = [];
+
+  playerNameArray.forEach(letter => {
+    displayArray.push(`<span>${letter}</span>`);
+  });
+
+  const displayName = { __html: displayArray.join("") };
+
+  return (
+    <header className="Header">
+      <h1 dangerouslySetInnerHTML={displayName}></h1>
+      <h2>
+        {player.score} point{pointsSuffix}
+      </h2>
+    </header>
+  );
+}
+
+function mapStateToProps(state) {
+  return state.mainReducer;
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
