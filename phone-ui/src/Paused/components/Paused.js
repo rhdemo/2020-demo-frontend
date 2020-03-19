453ef9d03041ce23f2758ea89248bf7cebf44417
Kyle Buchanan <kylebuch8@gmail.com>
2020-03-18 20:33:32 -0400

    Header and paused state updates

--- phone-ui/src/Paused/components/Paused.js
+++ phone-ui/src/Paused/components/Paused.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import Header from "../../Header";
+import MainContent from "../../MainContent";
+import "./Paused.scss";
 
-import './Paused.scss';
-
-function Paused() {
-
+function Paused({ player }) {
   return (
-    <div className='paused'>
-      <h1>Paused</h1>
+    <div className="paused">
+      <Header></Header>
+      <MainContent>
+        <h2>Paused</h2>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Object</th>
+                <th className="center">√</th>
+                <th className="center">X</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {player.history.map(round => (
+                <tr key={round.itemId}>
+                  <td>{round.itemName}</td>
+                  <td className="center">{round.right}</td>
+                  <td className="center">{round.wrong}</td>
+                  <td>{round.points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </MainContent>
     </div>
   );
 }
 
 function mapStateToProps(state) {
-  return state.appReducer;
+  return state.mainReducer;
 }
 
 function mapDispatchToProps(dispatch) {
   return {};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Paused);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Paused);
