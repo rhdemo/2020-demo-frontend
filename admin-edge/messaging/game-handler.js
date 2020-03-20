4a7993af209742e410e5d63557777fd0bfe5af83
Chris Chase <cchase@redhat.com>
2020-03-19 22:55:09 -0400

    Edge ignores game updates if already completed by another pod.

--- admin-edge/messaging/game-handler.js
+++ admin-edge/messaging/game-handler.js
@@ -3,15 +3,41 @@ const log = require('../utils/log')('messaging/game-handler');
 const Game = require("../models/game");
 
 async function gameHandler(message) {
+  log.info('incoming message', message);
+
+  const newGame = lodashGet(message, 'body.game');
+  if (!newGame) {
+    return;
+  }
+
+  if (isCurrent(newGame)) {
+    log.info('Ignoring game change, already updated.');
+    return;
+  }
+
   let game = new Game();
   game.updateAttributes(lodashGet(message, 'body.game'));
 
   try {
+    log.info('Updating game');
     await game.save();
     global.game = game;
   } catch (error) {
     log.error("Failed to update game. Error:", error);
   }
+}
+
+function isCurrent(game) {
+  if (global.game.id !== game.id) {
+    return false;
+  }
+  if (global.game.state !== game.state) {
+    return false;
+  }
+  if (global.game.date !== game.date) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = gameHandler;
