058bf326dc025b568bfd49f51eab530dc58f98c1
Kyle Buchanan <kylebuch8@gmail.com>
2020-03-24 20:57:21 -0400

    feat: game over screen (#24)
    * feat: game over screen
    
    * tightening space up
    
    * adding hints to handwriting and increasing stroke size

--- phone-ui/src/GameOver/components/GameOver.js
+++ phone-ui/src/GameOver/components/GameOver.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import Header from "../../Header";
+import History from "../../History";
+import "./GameOver.scss";
 
-import './GameOver.scss';
-
-function GameOver() {
-
+function GameOver({ game, player }) {
   return (
-    <div className='lobby'>
-      <h1>Game Over</h1>
+    <div className="GameOver">
+      <h1 className="title">Game Over</h1>
+      <Header></Header>
+      <table className="score-table">
+        <tbody>
+          <tr>
+            <td className="icon-cell">
+              <img
+                src={require("../../images/history-check.svg")}
+                alt="Correct guesses"
+                className="icon"
+              />
+            </td>
+            <td>Correct Guesses:</td>
+            <td className="total">{player.right}</td>
+          </tr>
+          <tr>
+            <td className="icon-cell">
+              <img
+                src={require("../../images/history-x.svg")}
+                alt="Incorrect guesses"
+                className="icon"
+              />
+            </td>
+            <td>Incorrect Guesses:</td>
+            <td className="total">{player.wrong}</td>
+          </tr>
+        </tbody>
+      </table>
+      <History></History>
     </div>
   );
 }
 
 function mapStateToProps(state) {
-  return state.appReducer;
+  return state.mainReducer;
 }
 
 function mapDispatchToProps(dispatch) {
   return {};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameOver);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameOver);
---
+++ phone-ui/src/History/components/History.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import "./History.scss";
+
+function History({ player }) {
+  return (
+    <div className="History">
+      <table>
+        <thead>
+          <tr>
+            <th>Object</th>
+            <th className="center">
+              <img
+                src={require("../../images/history-check.svg")}
+                alt="Correct guesses"
+                className="icon"
+              />
+            </th>
+            <th className="center">
+              <img
+                src={require("../../images/history-x.svg")}
+                alt="Incorrect guesses"
+                className="icon"
+              />
+            </th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {player.history.map(round => (
+            <tr key={round.itemId}>
+              <td>{round.itemName}</td>
+              <td className="center">{round.right}</td>
+              <td className="center">{round.wrong}</td>
+              <td>{round.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return state.mainReducer;
+}
+
+export default connect(mapStateToProps)(History);
