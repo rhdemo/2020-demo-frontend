fe7f01e3d0293e71808877d2370c81caa86fe348
Kyle Buchanan <kylebuch8@gmail.com>
2020-04-11 11:45:41 -0400

    adding the ability to dismiss the toast on the pause screen
    Fixes #28

--- phone-ui/src/Toast/components/Toast.js
+++ phone-ui/src/Toast/components/Toast.js
@@ -3,11 +3,7 @@ import React from "react";
 import "./Toast.scss";
 
 function Toast({ className, children }) {
-  return (
-    <div className={`Toast ${className}`}>
-      {children}
-    </div>
-  );
+  return <div className={`Toast ${className}`}>{children}</div>;
 }
 
 export default Toast;
