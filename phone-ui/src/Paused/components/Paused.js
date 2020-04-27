df550c9a4d5780009350e64eba190d3f9085a1c5
Kyle Buchanan <kylebuch8@gmail.com>
2020-04-27 15:02:43 -0400

    chris!!!

--- phone-ui/src/Paused/components/Paused.js
+++ phone-ui/src/Paused/components/Paused.js
@@ -10,7 +10,7 @@ import "./Paused.scss";
 
 function Paused({ player }) {
   const gameServerRef = useRef(player.gameServer);
-  const [toastClass, setToastClass] = useState("show");
+  const [toastClass, setToastClass] = useState("");
 
   useEffect(() => {
     if (gameServerRef.current !== player.gameServer) {
